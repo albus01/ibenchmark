bf5b13dfe5e9ab115f3245808c0ce2d951c16d01
shaheng <shaheng@baidu.com>
2015-06-05 14:22:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,114 +1,100 @@
 # iBenchmark
 A benckmark that can generate http(s)'s query by short and long connection
 
-一、工具介绍
-
-iBenchmark使用Go语言研发，为测试HTTPS Server的QPS、CPS性能指标而设计。最初版本只能测试HTTPS短连接，即CPS指标。故需要结合ab、wrk工具一起测试HTTPS Server的QPS、CPS指标。 现已改善，囊括了ab、wrk的特性，支持HTTP以及HTTPS的长连接、短连接，可测试HTTPS、HTTP的QPS、CPS性能指标。
-
-二、工具使用
-
+###一、工具介绍
+iBenchmark使用Go语言自研发，为测试HTTPS Server的QPS、CPS性能指标而设计。最初版本只能测试HTTPS短连接，即CPS指标。故需要结合ab、wrk工具一起测试HTTPS Server的QPS、CPS指标。
+现已改善，囊括了ab、wrk的特性，支持HTTP以及HTTPS的长连接、短连接，可测试HTTPS、HTTP的QPS、CPS性能指标。
+###二、工具使用
 使用帮助：
-
 Usage: iBenchmark [options]
-
 -H="Host: baike.baidu.com": request header
-
 -k=false: send request after handshake on the keep-alive connection
-
 -c=1: concurrency
-
 -h=false: show help
-
 -r=0: total requests per connection
-
 -s="TLS_RSA_WITH_RC4_128_SHA": cipher suite
-
 -t=0: timelimit (msec)
-
 -u="https://0.0.0.0:28080/": server url
-
-cihper suite: 
-TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA 
-TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA TLS_RSA_WITH_AES_256_CBC_SHA 
-TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA 
-TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 
-TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 TLS_FALLBACK_SCSV 
-TLS_RSA_WITH_3DES_EDE_CBC_SHA TLS_ECDHE_ECDSA_WITH_RC4_128_SHA 
-TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA 
-TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA TLS_RSA_WITH_RC4_128_SHA 
-TLS_RSA_WITH_AES_128_CBC_SHA TLS_ECDHE_RSA_WITH_RC4_128_SHA 
+cihper suite:
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA
+TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA
+TLS_RSA_WITH_AES_256_CBC_SHA
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
+TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+TLS_FALLBACK_SCSV
+TLS_RSA_WITH_3DES_EDE_CBC_SHA
+TLS_ECDHE_ECDSA_WITH_RC4_128_SHA
+TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA
+TLS_RSA_WITH_RC4_128_SHA
+TLS_RSA_WITH_AES_128_CBC_SHA
+TLS_ECDHE_RSA_WITH_RC4_128_SHA
 参数说明：
 
--k TCP(TLS)握手完成后保持长连接并在此长连接上发送r(r为参数-r)个Query请求，每个concurrency保持一个长连接。默认：关
--c 并发数。若使用-k，则-c的数量即为长连接的数量。默认：1
--t 每个连接持续的时间，持续时间内将持续发送请求，覆盖-r选项。默认：0
--r 每个连接上的请求数。注意：此参数与-t冲突，若指定了-t，则此选项无效。默认：0
--s 指定TLS握手时的chipersuite(加密套件)。默认：TLS_RSA_WITH_RC4_128_SHA
--u 服务的url。组成 protocol://hostname:port/path。protocol有http、https。默认：https://0.0.0.0:28080/，此时protocol：https，hostname:0.0.0.0，port：28080，path：/ 注：protocol要与port匹配。若要向HTTP发送压力，则需要将protocol改为http，port改为对应的端口，例如80。
--h 帮助。
--H 指定request Header头。使用方式 -H '["Host:baike.baidu.com","Connection:Keep-alive"]' 。注意格式：中括号外用单引号括起来，中括号内每个元素使用双引号"括起来，如果元素大于1个，元素间使用逗号隔开。不按此格式书写的-H将解析失败。
-三、使用案例
+- -k TCP(TLS)握手完成后保持长连接并在此长连接上发送r(r为参数-r)个Query请求，每个concurrency保持一个长连接。默认：关
+- -c 并发数。若使用-k，则-c的数量即为长连接的数量。默认：1
+- -t 每个连接持续的时间，持续时间内将持续发送请求，覆盖-r选项。默认：0
+- -r 每个连接上的请求数。注意：此参数与-t冲突，若指定了-t，则此选项无效。默认：0
+- -s 指定TLS握手时的chipersuite(加密套件)。默认：TLS_RSA_WITH_RC4_128_SHA
+- -u 服务的url。组成 protocol://hostname:port/path。protocol有http、https。默认：https://0.0.0.0:28080/，此时protocol：https，hostname:0.0.0.0，port：28080，path：/
+注：protocol要与port匹配。若要向HTTP发送压力，则需要将protocol改为http，port改为对应的端口，例如80。
+- -h 帮助。
+- -H 指定request Header头。使用方式 -H '["Host:baike.baidu.com","Connection:Keep-alive"]' 。注意格式：中括号外用单引号括起来，中括号内每个元素使用双引号"括起来，如果元素大于1个，元素间使用逗号隔开。不按此格式书写的-H将解析失败。
 
+###三、使用案例
 e.g. HTTPS QPS
-
 $./iBenchmark -c 2 -r 10 -u https://127.0.0.1:8800/shaheng.html -k -H '["Host:baike.baidu.com"]'
-
-Server Software:nginx/1.4.1
-
-Server Port:8800 Request
-
-Headers:
-
+benchmark start
+Server Software: nginx/1.4.1
+Server Hostname:127.0.0.1
+Server Port:8800
+Request Headers:
 Host:baike.baidu.com
 
 Document Path:/shaheng.html
-
 Document Length:131
-
 Concurrency:2
-
 Time Duration:36ms
-
 Avg Time Taken:3ms
-
 Complete Requests:20
-
 Failed Request:0
-
 Request Per Second:555
-
 Connections Per Second:0
-
 Non2XXCode:0
-由于使用了-k参数，故2个并发建立了两个长连接，每个长连接发送10个Request请求。此为一个QPS测试的案例。
-
-输出说明：
-
-Server Software为请求的后端服务器 Server Hostname为请求的Server HostName Server Port 请求的Server端口 Document Path 请求的路径 Document Length 服务端返回的文档的长度 Concurrency 并发数 Time Duration 从启动到结束经历的时间 Avg Time Taken 每个请求的平均响应时间 Complete Request 完成的请求数 Failed Request 失败的请求数 Request Per Second 每秒请求量 QPS (测试CPS时，此数据为0) Connections Per Second 每秒连接量 CPS(打开-k 测试QPS时为0) Non2XXCode 不是200~299之间的HTTP 状态码
+#####由于使用了-k参数，故2个并发建立了两个长连接，每个长连接发送10个Request请求。此为一个QPS测试的案例。
+####输出说明：
+Server Software为请求的后端服务器
+Server Hostname为请求的Server HostName
+Server Port 请求的Server端口
+Document Path 请求的路径
+Document Length 服务端返回的文档的长度
+Concurrency 并发数
+Time Duration 从启动到结束经历的时间
+Avg Time Taken 每个请求的平均响应时间
+Complete Request 完成的请求数
+Failed Request 失败的请求数
+Request Per Second 每秒请求量 QPS (测试CPS时，此数据为0)
+Connections Per Second 每秒连接量 CPS(打开-k 测试QPS时为0)
+Non2XXCode 不是200~299之间的HTTP 状态码
 
 e.g. HTTPS CPS
 
 ./iBenchmark -c 2 -t 5000 -u https://127.0.0.1:8800/shaheng.html -k -H '["Host:baike.baidu.com"]'
-
 Server Software:
-
 Server Hostname:127.0.0.1
-
 Server Port:8800
-
-Request Headers: ["Host:baike.baidu.com"]
-
+Request Headers:
+["Host:baike.baidu.com"]
 Document Path:/shaheng.html
-
+Document Length:0
 Concurrency:2
-
+Time Duration:5001ms
+Avg Time Taken:5ms
 Complete Requests:1917
-
 Failed Request:0
-
 Request Per Second:0
-
 Connections Per Second:383
-
 Non2XXCode:0
+
 此案例没有使用-k，即都为短连接。-t 5000运行了5000ms。-c 2 两个并发，每个并发持续建立连接、关闭连接，不发送query。为CPS的性能测试。
---
+++ iBenchmark.go
@@ -0,0 +1,394 @@
+/* iBenchmark.go - benchmark tool for Https/Http server */
+/*
+modification history
+--------------------
+2015/5/15, by Heng Sha, create
+Thanks to Sijie Yang
+*/
+/*
+DESCRIPTION
+*/
+package main
+
+import (
+	"bufio"
+	//"bytes"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	//"reflect"
+	"encoding/json"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const (
+	DC_BUCKET_SIZE = 10 // size of delay counter bucket (in ms)
+	DC_BUCKET_NUM  = 10 // number of delay counter bucket
+
+	HTTP_CODE_BUCKET_SIZE = 100
+	HTTP_CODE_BUCKET_NUM  = 6
+)
+
+var CipherSuites = map[string]uint16{
+	"TLS_RSA_WITH_RC4_128_SHA":                uint16(0x0005),
+	"TLS_RSA_WITH_3DES_EDE_CBC_SHA":           uint16(0x000a),
+	"TLS_RSA_WITH_AES_128_CBC_SHA":            uint16(0x002f),
+	"TLS_RSA_WITH_AES_256_CBC_SHA":            uint16(0x0035),
+	"TLS_ECDHE_ECDSA_WITH_RC4_128_SHA":        uint16(0xc007),
+	"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":    uint16(0xc009),
+	"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":    uint16(0xc00a),
+	"TLS_ECDHE_RSA_WITH_RC4_128_SHA":          uint16(0xc011),
+	"TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA":     uint16(0xc012),
+	"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":      uint16(0xc013),
+	"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":      uint16(0xc014),
+	"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":   uint16(0xc02f),
+	"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256": uint16(0xc02b),
+
+	// TLS_FALLBACK_SCSV isn't a standard cipher suite but an indicator
+	// that the client is doing version fallback. See
+	// https://tools.ietf.org/html/draft-ietf-tls-downgrade-scsv-00.
+	"TLS_FALLBACK_SCSV": uint16(0x5600),
+}
+
+var (
+	help        *bool   = flag.Bool("h", false, "show help")
+	url         *string = flag.String("u", "https://0.0.0.0:28080/", "server url")
+	concurrency *int    = flag.Int("c", 1, "concurrency")
+	reqNum      *int    = flag.Int("r", 0, "total requests per connection")
+	dur         *int    = flag.Int("t", 0, "timelimit (msec)")
+	withReq     *bool   = flag.Bool("k", false, "send request after handshake on the keep-alive connection")
+	cipherSuite *string = flag.String("s", "TLS_RSA_WITH_RC4_128_SHA", "cipher suite")
+	header      *string = flag.String("H", "", "request header")
+)
+
+var (
+	proto     string
+	host      string
+	port      string
+	address   string
+	path      string
+	swithHttp bool = false
+	interval  int
+	network   string          = "tcp"
+	servers   map[string]bool = make(map[string]bool)
+	headers   []string
+)
+
+type Reporter struct {
+	Server              string
+	Hostname            string
+	Port                string
+	Path                string
+	Headers             string
+	ContentLength       int64
+	Concurrency         int
+	TimeTaken           int64
+	TimeDur             int64
+	TotalRequest        int
+	FailedRequest       int
+	RequestPerSecond    int
+	ConnectionPerSecond int
+	Non2XXCode          int
+}
+
+func (r *Reporter) Printer() error {
+	report := fmt.Sprintf("Server Software:%s\nServer Hostname:%s\nServer Port:%s\n\nRequest Headers:\n%s\n\nDocument Path:%s\nDocument Length:%d\n\nConcurrency:%d\nTime Duration:%dms\nAvg Time Taken:%dms\n\nComplete Requests:%d\nFailed Request:%d\n\nRequest Per Second:%d\nConnections Per Second:%d\n\nNon2XXCode:%d\n\n", r.Server, r.Hostname, r.Port, r.Headers, r.Path, r.ContentLength, r.Concurrency, r.TimeDur, r.TimeTaken/1000/int64(r.TotalRequest), r.TotalRequest, r.FailedRequest, r.RequestPerSecond, r.ConnectionPerSecond, r.Non2XXCode)
+	fmt.Println(report)
+	return nil
+}
+
+func printHelp() {
+	fmt.Println("Usage: iBenchmark [options]")
+	flag.PrintDefaults()
+	fmt.Printf("\ncihper suite:\n")
+	for k := range CipherSuites {
+		fmt.Printf("  %s\n", k)
+	}
+}
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+			printHelp()
+		}
+	}()
+	interval = 3600
+	flag.Parse()
+	if *help {
+		printHelp()
+		return
+	}
+	proto = (*url)[:strings.Index(*url, ":")]
+	if proto == "http" {
+		swithHttp = true
+	}
+	host = (*url)[strings.Index(*url, "//")+2 : strings.LastIndexAny(*url, ":")]
+	port = (*url)[strings.LastIndex(*url, ":")+1 : strings.LastIndex(*url, "/")]
+	address = host + ":" + port
+	path = (*url)[strings.LastIndex(*url, "/"):]
+	if host == "" || port == "" || path == "" || proto == "" {
+		printHelp()
+		return
+	}
+	runtime.GOMAXPROCS(8)
+
+	timeout := time.Duration(*dur) * time.Millisecond
+	finChan := make([]chan bool, *concurrency)
+
+	// number of connections to crypto server cluster
+	reporter := new(Reporter)
+	reporter.Concurrency = *concurrency
+	reporter.Hostname = host
+	reporter.Port = port
+	reporter.Path = path
+
+	fmt.Println("benchmark start ")
+	// start workers
+	start := time.Now()
+	for i := 0; i < *concurrency; i = i + 1 {
+		finChan[i] = make(chan bool)
+		go worker(*reqNum, timeout, reporter, finChan[i], i)
+	}
+
+	// wait for finish
+	for i := 0; i < *concurrency; i = i + 1 {
+		switch {
+		case <-(finChan[i]):
+			continue
+		}
+	}
+	duration := time.Since(start).Nanoseconds() / (1000 * 1000)
+	reporter.TimeDur = duration
+	if *withReq {
+		reporter.RequestPerSecond = int(float64(reporter.TotalRequest) / (float64(reporter.TimeDur) / 1000))
+		reporter.ConnectionPerSecond = 0
+	} else {
+		reporter.ConnectionPerSecond = int(float64(reporter.TotalRequest) / (float64(reporter.TimeDur) / 1000))
+		reporter.RequestPerSecond = 0
+	}
+	var server string
+	for key, _ := range servers {
+		server = fmt.Sprintf("%s %s", server, key)
+	}
+	reporter.Server = server
+	reporter.Headers = *header
+	time.Sleep(1 * time.Second)
+	reporter.Printer()
+}
+
+func (r *Reporter) GetResponse(conn *net.Conn) error {
+	var resp *http.Response
+	var err error
+	cipher := CipherSuites[*cipherSuite]
+	procStart := time.Now()
+	r.TotalRequest += 1
+	if !swithHttp {
+		if !*withReq {
+			resp, err = HTTPSGet(cipher)
+		} else {
+			resp, err = HTTPSGet_KeepAlive(cipher, conn)
+		}
+
+	} else {
+		if !*withReq {
+			resp, err = HTTPGet()
+		} else {
+			resp, err = HTTPGet_KeepAlive(conn)
+		}
+	}
+	if err != nil {
+		fmt.Println(fmt.Sprintf("HTTP(S) GET ERROR %v", err))
+		r.FailedRequest += 1
+	}
+	if resp != nil {
+		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+			r.Non2XXCode += 1
+		}
+		r.ContentLength = resp.ContentLength
+		for _, server := range resp.Header["Server"] {
+			if !servers[server] {
+				servers[server] = true
+			}
+		}
+	}
+	end := time.Now()
+	elapse := end.Sub(procStart).Nanoseconds() / 1000
+	r.TimeTaken += elapse
+	return err
+}
+
+func worker(reqNum int, timeout time.Duration, reporter *Reporter, finChan chan bool, index int) {
+	end_time := time.After(timeout)
+	var conn net.Conn
+
+	defer func() {
+		if conn != nil {
+			conn.Close()
+
+		}
+	}()
+	if *dur != 0 {
+		for {
+			select {
+			case <-end_time:
+				finChan <- true
+				return
+			default:
+				err := reporter.GetResponse(&conn)
+				if err != nil {
+					fmt.Println(fmt.Sprintf("[ERROR]:%s", err))
+					conn = nil
+				}
+
+			}
+		}
+
+	} else {
+		for i := 0; i < reqNum; i++ {
+			err := reporter.GetResponse(&conn)
+			if err != nil {
+				fmt.Println(fmt.Sprintf("[ERROR]:%s", err))
+				conn = nil
+			}
+		}
+		finChan <- true
+		return
+	}
+
+}
+
+func HTTPSGet(cipherSuite uint16) (*http.Response, error) {
+	// create tls config
+	config := tls.Config{
+		InsecureSkipVerify:     true,
+		SessionTicketsDisabled: true,
+		CipherSuites:           []uint16{cipherSuite},
+	}
+	// connect to tls server
+	conn, err := tls.Dial(network, address, &config)
+	if err != nil {
+		fmt.Errorf("client: dial: %s", err)
+		return nil, err
+	}
+	if *withReq {
+		return SendQuery(conn)
+	} else {
+		return nil, nil
+	}
+}
+
+func HTTPSGet_KeepAlive(cipherSuite uint16, conn *net.Conn) (*http.Response, error) {
+	// create tls config
+	config := tls.Config{
+		InsecureSkipVerify:     true,
+		SessionTicketsDisabled: true,
+		CipherSuites:           []uint16{cipherSuite},
+	}
+	var err error
+	// connect to tls server
+	if *conn == nil {
+		//fmt.Println(fmt.Sprintf("[INFO] conn is nil"))
+		*conn, err = tls.Dial(network, address, &config)
+		if err != nil {
+			fmt.Errorf("client: dial: %s", err)
+			return nil, err
+		}
+
+	}
+	resp, err := SendQuery(*conn)
+	return resp, err
+}
+
+func HTTPGet() (*http.Response, error) {
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		fmt.Errorf("client: dial: %s", err)
+		return nil, err
+	}
+
+	if *withReq {
+		return SendQuery(conn)
+	} else {
+		return nil, nil
+	}
+}
+
+func HTTPGet_KeepAlive(conn *net.Conn) (*http.Response, error) {
+	var err error
+	if *conn == nil {
+		*conn, err = net.Dial(network, address)
+		if err != nil {
+			fmt.Errorf("client: dial: %s", err)
+			return nil, err
+		}
+
+	}
+	resp, err := SendQuery(*conn)
+	return resp, err
+}
+
+func SendQuery(conn net.Conn) (*http.Response, error) {
+	//procStart := time.Now()
+	var resp *http.Response
+	var err error
+	// send message
+	//message := "GET /shaheng.html HTTP/1.1\r\nHost: baike.baidu.com\r\n\r\n"
+	var message string
+	var temp string
+	if *header != "" {
+		json.Unmarshal([]byte(*header), &headers)
+		for _, h := range headers {
+			temp = fmt.Sprintf("%s\r\n%s", h, temp)
+		}
+		*header = temp
+		message = fmt.Sprintf("GET %s HTTP/1.1\r\n%s\r\n", path, temp)
+		//fmt.Println(fmt.Sprintf("%q", message))
+	} else {
+		message = fmt.Sprintf("GET %s HTTP/1.1\r\n\r\n", path)
+	}
+	n, err := io.WriteString(conn, message)
+	if err != nil {
+		fmt.Errorf("client: write: %s", err)
+		return nil, err
+	}
+	//fmt.Printf("client: wrote %q (%d bytes), wait for reply", message, n)
+	// read message
+	reply := make([]byte, 2560)
+	n, err = conn.Read(reply)
+	req := &http.Request{Method: "GET"}
+	resp, err = http.ReadResponse(bufio.NewReader(strings.NewReader(string(reply[:n]))), req)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+	//fmt.Printf("ContentLengh:%d\n", resp.ContentLength)
+	//fmt.Printf("Proto:%s\n", resp.Proto)
+	//fmt.Printf("StatusCode:%d\n", resp.StatusCode)
+	//fmt.Printf("Status:%s\n", resp.Status)
+	//fmt.Printf("KeepAlive:%v\n", resp.Close)
+	////var buf bytes.Buffer
+	////io.Copy(&buf, resp.Body)
+	////fmt.Printf("Body:%s\n", buf.String())
+	////fmt.Printf("Header:%s\n", resp.Header["Date"])
+	//fmt.Println("Request Headers:")
+	//request := reflect.ValueOf(resp.Request).Elem()
+	//for i := 0; i < request.NumField(); i++ {
+	//	fmt.Printf("%s=%v;", request.Type().Field(i).Name, request.Field(i).Interface())
+	//}
+	//fmt.Println()
+	//fmt.Printf("Headers:%s", resp.Header["Server"])
+	//for key, value := range resp.Header {
+	//	fmt.Println("key:", key, "value:", value)
+	//}
+	//fmt.Println()
+
+	//elapse := time.Now().Sub(procStart).Nanoseconds() / 1000
+	//fmt.Printf("GET transaction elapse %d us\n", elapse)
+	//fmt.Println()
+	return resp, err
+}
